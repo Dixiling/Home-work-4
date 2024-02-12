09e13a2435847f93c3c5b891b02fe9203f1d88a4
Конпаку <sputnik-999-mel@mail.ru>
2024-02-12 12:08:06 +0500

    binarycalculayirs

---
+++ BinaryCalculators.java
@@ -0,0 +1,26 @@
+public class BinaryCalculators<B> implements Calculators {
+    @Override
+    public B sum() {
+        return null;
+    }
+
+    @Override
+    public B mult() {
+        return null;
+    }
+
+    @Override
+    public B div() {
+        return null;
+    }
+
+    @Override
+    public B fromBinary() {
+        return null;
+    }
+
+    @Override
+    public String toBinary(Object number) {
+        return null;
+    }
+}
